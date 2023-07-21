d619d70e1872a8519a347b5cec5487729c217c77
Erik Pedersen <109049703+basementlivin@users.noreply.github.com>
2023-07-21 09:22:41 -0400

    Successfully link to Sanity Studio :)

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,27 @@
-export default function IndexPage({ pets }) {
+import { createClient } from "next-sanity";
+
+export default function IndexPage({ authors }) {
   return (
     <>
       <header>
         <h1>Sanity + Next.js</h1>
       </header>
       <main>
-        <h2>Pets</h2>
-        {pets.length > 0 && (
+        <h2>authors</h2>
+        {authors.length > 0 && (
           <ul>
-            {pets.map((pet) => (
-              <li key={pet._id}>{pet?.name}</li>
+            {authors.map((author) => (
+              <li key={author._id}>{author?.name}</li>
             ))}
           </ul>
         )}
-        {!pets.length > 0 && <p>No pets to show</p>}
-        {pets.length > 0 && (
+        {!authors.length > 0 && <p>No authors to show</p>}
+        {authors.length > 0 && (
           <div>
-            <pre>{JSON.stringify(pets, null, 2)}</pre>
+            <pre>{JSON.stringify(authors, null, 2)}</pre>
           </div>
         )}
-        {!pets.length > 0 && (
+        {!authors.length > 0 && (
           <div>
             <div>¯\_(ツ)_/¯</div>
             <p>
@@ -32,22 +34,19 @@ export default function IndexPage({ pets }) {
     </>
   );
 }
+const client = createClient({
+  projectId: "uhn6b07j",
+  dataset: "production",
+  apiVersion: "2023-07-21",
+  useCdn: false
+});
 
 export async function getStaticProps() {
-  const pets = [
-    /* {
-      _createdAt: "2022-03-08T09:28:00Z",
-      _id: "1f69c53d-418a-452f-849a-e92466bb9c75",
-      _rev: "xnBg0xhUDzo561jnWODd5e",
-      _type: "pet",
-      _updatedAt: "2022-03-08T09:28:00Z",
-      name: "Bamse"
-    } */
-  ];
+  const authors = await client.fetch('*[_type == "author"]');
 
   return {
     props: {
-      pets
+      authors
     }
   };
 }
